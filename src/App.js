fbf2c8752129a5da759dc998ea2ed224d20ce190
GhitaB <ghitabizau27@gmail.com>
2023-01-12 20:28:07 +0200

    Enable/disable buttons based on board current position.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -19,13 +18,44 @@ function App() {
   const block_type = (row, col, a_type) => {
     const block = {0: 'empty', 1: 'wall', 2: 'start', 3: 'flag'}[a_type];
     let class_name = block;
-    if(current_position.row == row && current_position.col == col) {
+    if(current_position.row === row && current_position.col === col) {
       class_name += " now";
-      console.log("DDDDDDDDDD");
     }
     return class_name;
   };
 
+
+  const check_direction = (direction) => {
+    let row = current_position.row;
+    let col = current_position.col;
+
+    if (direction === 'up') {
+      row -= 1;
+    }
+
+    if (direction === 'down') {
+      row += 1;
+    }
+
+    if (direction === 'left') {
+      col -= 1;
+    }
+
+    if (direction === 'right') {
+      col += 1;
+    }
+
+    return board[row][col];
+  }
+
+  const button_class = (direction) => {
+    const check = check_direction(direction);
+    if (check === 1) {
+      return "disabled";
+    } else {
+      return "enabled";
+    }
+  }
 
   return (
     <div className="App">
@@ -42,10 +72,10 @@ function App() {
       </div>
 
       <div className="controls">
-        <button>Up</button>
-        <button>Down</button>
-        <button>Left</button>
-        <button>Right</button>
+        <button className={button_class("up")}>Up</button>
+        <button className={button_class("down")}>Down</button>
+        <button className={button_class("left")}>Left</button>
+        <button className={button_class("right")}>Right</button>
       </div>
 
     </div>
